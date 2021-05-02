61bb5373799f953b28f852dc4c9312265a1662f0
Artem Smotrakov <artem.smotrakov@gmail.com>
2021-05-02 11:44:48 +0200

    Added SaferRmiDeserializationWithFilter and DeserializationConfig

---
+++ main/src/main/java/com/gypsyengineer/ql/fun/java/rmi/SaferRmiDeserializationWithFilter.java
@@ -0,0 +1,25 @@
+package com.gypsyengineer.ql.fun.java.rmi;
+
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.security.Security;
+
+public class SaferRmiDeserializationWithFilter {
+
+    public static void main(String[] args)
+            throws RemoteException, MalformedURLException, AlreadyBoundException {
+
+        DeserializationConfig.setDeserializationFilterIfNecessary();
+
+        System.out.print("[+] Starting a local registry ... ");
+        LocateRegistry.createRegistry(1099);
+        System.out.println("okay");
+
+        System.out.print("[+] Bind an object ... ");
+        Naming.bind("echo", new Echo());
+        System.out.println("okay");
+    }
+}
