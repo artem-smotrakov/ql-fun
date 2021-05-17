8f933692c2ab48c1264eb1ecf31e8ae027d05ea3
Artem Smotrakov <artem.smotrakov@gmail.com>
2021-05-17 08:48:07 +0200

    More tests for Jackson deserialization

--- main/src/main/java/com/gypsyengineer/ql/fun/jackson/two/SaferCatDeserialization.java
+++ main/src/main/java/com/gypsyengineer/ql/fun/jackson/two/SaferCatDeserialization.java
@@ -11,22 +11,22 @@ import static com.gypsyengineer.ql.fun.Util.withSocket;
 
 public class SaferCatDeserialization {
 
-    private static final String command =
+    private static final String COMMAND =
             System.getProperty("os.name").toLowerCase().contains("windows")
                     ? "calc.exe" : "gedit";
 
-    private static final String good =
+    private static final String GOOD_CAT =
             "{\"name\":\"Dude\"," +
                     "\"tag\":[" +
                     "   \"com.gypsyengineer.ql.fun.jackson.two.Tag\"," +
                     "   {\"title\":\"123\"}" +
                     "]}";
 
-    private static final String bad =
+    private static final String BAD_CAT =
             "{\"name\":\"Dude\"," +
                     "\"tag\":[" +
                     "   \"com.popular.lib.Exec\"," +
-                    "   {\"command\":\"" + command + "\"}" +
+                    "   {\"command\":\"" + COMMAND + "\"}" +
                     "]}";
 
     private static <T> T deserializeSafe(String string, Class<T> clazz) throws IOException {
@@ -45,10 +45,10 @@ public class SaferCatDeserialization {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(deserializeSafe(good, Cat.class));
+        System.out.println(deserializeSafe(GOOD_CAT, Cat.class));
 
         try {
-            System.out.println(deserializeSafe(bad, Cat.class));
+            System.out.println(deserializeSafe(BAD_CAT, Cat.class));
         } catch (Exception e) {
             System.out.println("Deserialization failed: " + e);
         }
