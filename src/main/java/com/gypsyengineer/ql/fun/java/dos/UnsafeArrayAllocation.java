de2d62390d69ed6b39eb4e3dea03d1614a42b3c7
Artem Smotrakov <artem.smotrakov@gmail.com>
2021-07-09 13:59:08 +0200

    Updated UnsafeArrayAllocation.java

--- src/main/java/com/gypsyengineer/ql/fun/java/dos/UnsafeArrayAllocation.java
+++ src/main/java/com/gypsyengineer/ql/fun/java/dos/UnsafeArrayAllocation.java
@@ -1,7 +1,6 @@
 package com.gypsyengineer.ql.fun.java.dos;
 
 import com.google.common.primitives.Ints;
-import com.google.common.primitives.Shorts;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.DataInputStream;
@@ -19,53 +18,42 @@ public class UnsafeArrayAllocation {
 
     private static final int MAX_SIZE = 100;
 
-    // BAD: array size comes from remote user input
     public void unsafeArrayAllocation(Socket socket) throws IOException {
         try (InputStream is = socket.getInputStream()) {
             byte[] data = new byte[16];
             int n = is.read(data);
 
-            String[] arr1 = new String[data[0] * data[1]];
+            String[] arr1 = new String[data[0] * data[1]]; // BAD
 
             DataInputStream dis = new DataInputStream(is);
-            int[] arr2 = new int[dis.readShort()];
-            int[] arr3 = new int[dis.readInt()];
+            int[] arr2 = new int[dis.readInt()]; // BAD
 
             String s = new String(data);
-            long[] arr4 = new long[Short.parseShort(s)];
-            long[] arr5 = new long[Short.valueOf(s)];
-            long[] arr6 = new long[Integer.parseInt(s)];
-            long[] arr7 = new long[Integer.valueOf(s)];
+            long[] arr3 = new long[Integer.parseInt(s)]; // BAD;
+            long[] arr4 = new long[Integer.valueOf(s)]; // BAD
 
-            Object[] arr8 = new Object[new BigInteger(data).shortValue()];
-            Object[] arr9 = new Object[new BigInteger(data).shortValueExact()];
-            Object[] arr10 = new Object[new BigInteger(1, data).intValue()];
-            Object[] arr11 = new Object[new BigInteger(1, data).intValueExact()];
+            Object[] arr5 = new Object[new BigInteger(1, data).intValue()]; // BAD
+            Object[] arr6 = new Object[new BigInteger(1, data).intValueExact()]; // BAD
 
-            String[][] arr12 = new String[10][NumberUtils.createNumber(s).intValue()];
-            String[][] arr13 = new String[10][NumberUtils.createInteger(s).intValue()];
-            String[][] arr14 = new String[10][NumberUtils.toShort(s)];
-            String[][] arr15 = new String[10][NumberUtils.toInt(s)];
+            String[][] arr7 = new String[10][NumberUtils.createNumber(s).intValue()]; // BAD
+            String[][] arr8 = new String[10][NumberUtils.createInteger(s).intValue()]; // BAD
+            String[][] arr9 = new String[10][NumberUtils.toInt(s)]; // BAD
 
-            String[] arr16 = new String[Ints.fromByteArray(data)];
-            String[] arr17 = new String[Ints.fromBytes(data[0], data[1], data[2], data[3])];
-            String[] arr18 = new String[Ints.tryParse(s)];
-            String[] arr19 = new String[Shorts.fromByteArray(data)];
-            String[] arr20 = new String[Shorts.fromBytes(data[0], data[1])];
+            String[] arr10 = new String[Ints.fromByteArray(data)]; // BAD
+            String[] arr11 = new String[Ints.fromBytes(data[0], data[1], data[2], data[3])]; // BAD
+            String[] arr12 = new String[Ints.tryParse(s)]; // BAD
 
-            byte[] arr21 = new byte[ByteBuffer.wrap(data).getShort()];
-            byte[] arr22 = new byte[ByteBuffer.wrap(data).getInt()];
+            byte[] arr13 = new byte[ByteBuffer.wrap(data).getInt()]; // BAD
         }
     }
 
-    // BAD: size of byte buffer comes from remote user input
     public void unsafeByteBufferAllocation(Socket socket) throws IOException {
         try (InputStream is = socket.getInputStream()) {
             byte[] data = new byte[16];
             int n = is.read(data);
             int size = ByteBuffer.wrap(data).getInt();
-            ByteBuffer.allocate(size);
-            ByteBuffer.allocateDirect(size);
+            ByteBuffer.allocate(size); // BAD
+            ByteBuffer.allocateDirect(size); // BAD
         }
     }
 
@@ -79,18 +67,43 @@ public class UnsafeArrayAllocation {
         }
     }
 
-    // BAD: list capacity comes from remote user input
+    // GOOD: size of byte buffer comes from remote user input, but it is sanitized with a custom check
+    public void safeByteBufferAllocationWithCustomCheckOne(Socket socket) throws IOException {
+        try (InputStream is = socket.getInputStream()) {
+            byte[] data = new byte[16];
+            int n = is.read(data);
+            int size = ByteBuffer.wrap(data).getInt();
+            if (size > MAX_SIZE) {
+                throw new IllegalArgumentException("Too much!");
+            }
+            ByteBuffer.allocate(size);
+        }
+    }
+
+    // GOOD: size of byte buffer comes from remote user input, but it is sanitized with a custom check
+    public void safeByteBufferAllocationWithCustomCheckTwo(Socket socket) throws IOException {
+        try (InputStream is = socket.getInputStream()) {
+            byte[] data = new byte[16];
+            int n = is.read(data);
+            int size = ByteBuffer.wrap(data).getInt();
+            if (MAX_SIZE < size) {
+                return;
+            }
+            ByteBuffer.allocate(size);
+        }
+    }
+
     public void unsafeCollectionAllocation(Socket socket) throws IOException {
         try (InputStream is = socket.getInputStream()) {
             byte[] data = new byte[16];
             is.read(data);
             int size = ByteBuffer.wrap(data).getInt();
 
-            List<Object> list1 = new ArrayList<>(size);
-            List<Object> list2 = new Vector<>(size);
-            Set<Object> set1 = new HashSet<>(size);
-            Map<Object, Object> map1 = new HashMap<>(size);
-            Map<Object, Object> map2 = new ConcurrentHashMap<>(size);
+            List<Object> list1 = new ArrayList<>(size); // BAD
+            List<Object> list2 = new Vector<>(size); // BAD
+            Set<Object> set1 = new HashSet<>(size); // BAD
+            Map<Object, Object> map1 = new HashMap<>(size); // BAD
+            Map<Object, Object> map2 = new ConcurrentHashMap<>(size); // BAD
         }
     }
 
@@ -106,9 +119,11 @@ public class UnsafeArrayAllocation {
     }
 
     public static void main(String... args) {
-        List<Object> list = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            list.add(new ArrayList<>(Integer.MAX_VALUE));
-        }
+        int n = (read() << 24) + (read() << 16) + (read() << 8) + read();
+        ByteBuffer.allocate(n);
+    }
+
+    public static int read() {
+        return 0b01111111;
     }
 }
